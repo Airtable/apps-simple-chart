ba5970b8e8917b86dfeebd5d7fdb9191f9d97803
Airtable <blocks@airtable.com>
2020-02-28 22:47:24 +0000

    @airtable/simple-chart-block@2.0.0

---
+++ frontend/index.js
@@ -0,0 +1,135 @@
+import {
+    initializeBlock,
+    useBase,
+    useRecords,
+    useGlobalConfig,
+    TablePickerSynced,
+    ViewPickerSynced,
+    FieldPickerSynced,
+    Box,
+    FormField,
+} from '@airtable/blocks/ui';
+import React from 'react';
+
+// This block uses chart.js and the react-chartjs-2 packages.
+// Install them by running this in the terminal:
+// npm install chart.js react-chartjs-2
+import {Bar} from 'react-chartjs-2';
+
+const GlobalConfigKeys = {
+    TABLE_ID: 'tableId',
+    VIEW_ID: 'viewId',
+    X_FIELD_ID: 'xFieldId',
+};
+
+function SimpleChartBlock() {
+    const base = useBase();
+    const globalConfig = useGlobalConfig();
+
+    const tableId = globalConfig.get(GlobalConfigKeys.TABLE_ID);
+    const table = base.getTableByIdIfExists(tableId);
+
+    const viewId = globalConfig.get(GlobalConfigKeys.VIEW_ID);
+    const view = table ? table.getViewByIdIfExists(viewId) : null;
+
+    const xFieldId = globalConfig.get(GlobalConfigKeys.X_FIELD_ID);
+    const xField = table ? table.getFieldByIdIfExists(xFieldId) : null;
+
+    const records = useRecords(view);
+
+    const data = records && xField ? getChartData({records, xField}) : null;
+
+    return (
+        <Box
+            position="absolute"
+            top={0}
+            left={0}
+            right={0}
+            bottom={0}
+            display="flex"
+            flexDirection="column"
+        >
+            <Settings table={table} />
+            {data && (
+                <Box position="relative" flex="auto" padding={3}>
+                    <Bar
+                        data={data}
+                        options={{
+                            maintainAspectRatio: false,
+                            scales: {
+                                yAxes: [
+                                    {
+                                        ticks: {
+                                            beginAtZero: true,
+                                        },
+                                    },
+                                ],
+                            },
+                            legend: {
+                                display: false,
+                            },
+                        }}
+                    />
+                </Box>
+            )}
+        </Box>
+    );
+}
+
+function getChartData({records, xField}) {
+    const recordsByXValueString = new Map();
+    for (const record of records) {
+        const xValue = record.getCellValue(xField);
+        const xValueString = xValue === null ? null : record.getCellValueAsString(xField);
+
+        if (!recordsByXValueString.has(xValueString)) {
+            recordsByXValueString.set(xValueString, [record]);
+        } else {
+            recordsByXValueString.get(xValueString).push(record);
+        }
+    }
+
+    const labels = [];
+    const points = [];
+    for (const [xValueString, records] of recordsByXValueString.entries()) {
+        const label = xValueString === null ? 'Empty' : xValueString;
+        labels.push(label);
+        points.push(records.length);
+    }
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                backgroundColor: '#4380f1',
+                data: points,
+            },
+        ],
+    };
+    return data;
+}
+
+function Settings({table}) {
+    return (
+        <Box display="flex" padding={3} borderBottom="thick">
+            <FormField label="Table" width="33.33%" paddingRight={1} marginBottom={0}>
+                <TablePickerSynced globalConfigKey={GlobalConfigKeys.TABLE_ID} />
+            </FormField>
+            {table && (
+                <FormField label="View" width="33.33%" paddingX={1} marginBottom={0}>
+                    <ViewPickerSynced table={table} globalConfigKey={GlobalConfigKeys.VIEW_ID} />
+                </FormField>
+            )}
+            {table && (
+                <FormField label="X-axis field" width="33.33%" paddingLeft={1} marginBottom={0}>
+                    <FieldPickerSynced
+                        table={table}
+                        globalConfigKey={GlobalConfigKeys.X_FIELD_ID}
+                    />
+                </FormField>
+            )}
+        </Box>
+    );
+}
+
+initializeBlock(() => <SimpleChartBlock />);
